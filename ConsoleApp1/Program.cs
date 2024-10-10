a982900b1bbcd3772a449488eeaa28b6b6605e6d
alicalastikova <alicalastikova12@gmail.com>
2024-10-10 07:27:38 +0200

    alica12

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Random random = new Random();
+        int vygenerovanecislo = random.Next(maxValue: 100);
+
+
+        Console.WriteLine("cislo");
+
+        while (true)
+        {
+            string X = Console.ReadLine();
+            int attermps = 5;
+            int Y = int.Parse(X);
+
+
+            if (Y < vygenerovanecislo)
+            {
+                Console.WriteLine("tvoje cislo je mensie");
+            }
+            if (Y > vygenerovanecislo)
+            {
+                Console.WriteLine("tvoje cislo je vecsie");
+            }
+            if (Y == vygenerovanecislo)
+            {
+                Console.WriteLine("uhadol si cislo");
+            }
+
+        }
+    }
+
+
+
+
+
+}
---
+++ domaca_uloha/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace MyApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            SpocitajSamohlasky();
+        }
+        public static void SpocitajSamohlasky()
+        {
+            Console.WriteLine("write text");
+            string text = Console.ReadLine();
+            int length = text.Length;
+            Console.WriteLine("text length:" + length);
+            int vowelCount = 0;
+            char[] vowel ={ 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+            {
+                vowelCount++;
+                Console.WriteLine($"Pocet samohlasok: {vowelCount}");
+            }
+        }
+
+    }
+}

ee75cb252f6c97018da0b3799a2fd7fd2bc3d25f
alicalastikova <alicalastikova12@gmail.com>
2024-10-17 09:19:13 +0200

    TitaniusOcelChuj

--- cvicenie_OOP_Hra/Player.cs
+++ cvicenie_OOP_Hra/Player.cs
@@ -13,19 +13,49 @@ namespace cvicenie_OOP_Hra
         public int HP { get; set; }
         public int Mana { get; set; }
         public int AttackPower { get; set; }
+        public int CritChange { get; set; }
 
         public void DamagePlayer(Player player)
         {
+            int damageMultiPlayer = 1;
+
+            Random random = new Random();   
+            int randomNumber = random.Next(0,100);
+            if (randomNumber <= this.CritChange ) 
+            {
+                damageMultiPlayer = 2;
+                Console.WriteLine(this.Name+ "dal kriticky zasah.");
+            }
+
+
             int HPofEnemy = player.HP;
-            int AttackOfCurrentPlayer = this.AttackPower;
+            int AttackOfCurrentPlayer = this.AttackPower * damageMultiPlayer;
             int HPofEnemyAfterFight = HPofEnemy - AttackOfCurrentPlayer;
             player.HP = HPofEnemyAfterFight;
 
-
-
-
+        }
+        public bool Heal() 
+        {
+            if (this.Mana > 0)
+            {
+                int HPOPlayer = this.HP;
+                int ManaOfPlayer = this.Mana;
+                int HPOPlayerAfterHeal = HPOPlayer + ManaOfPlayer;
+                this.Mana = 0;
+                this.HP = HPOPlayerAfterHeal;
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
         }
 
+        public void RefilMana(int newMana)
+        { 
+            this.Mana = newMana;
 
+            
+        }
     }
 }
--- cvicenie_OOP_Hra/Program.cs
+++ cvicenie_OOP_Hra/Program.cs
@@ -6,39 +6,84 @@
         {
             Player Player = new Player();
             Player.Name = "TitaniusOcelChuj";
-            Player.HP = 30;
+            Player.HP = 70;
             Player.Mana = 10;
             Player.AttackPower = 5;
+            Player.CritChange = 50;
+            
 
             Player Player2 = new Player();
-            Player2.Name = "octopus";
-            Player2.HP = 30;
+            Player2.Name = "Octopus";
+            Player2.HP = 65;
             Player2.Mana = 10;
             Player2.AttackPower = 5;
+            Player2.CritChange = 10;
 
 
             while (Player.HP >= 0 && Player2.HP >= 0)
             {
-                Console.WriteLine("octopus:" + Player2.HP);
-                Console.WriteLine("TitaniusOcelChuj:" + Player2.HP);
+                Console.WriteLine("Octopus:" + Player2.HP);
+                Console.WriteLine("TitaniusOcelChuj:" + Player.HP);
                 Player.DamagePlayer(Player2);
                 Player2.DamagePlayer(Player);
-                
 
+                if (Player.HP <= 20)
+                {
+                    bool wasHealed = Player.Heal();
+                    if (wasHealed) 
+                    {
+                        Console.WriteLine("TitaniusOcelChuj postava bola uzdravena.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("TitaniusOcelChuj nema manu nebola uzdravena.");
+                    }
+
+                }
+                if (Player2.HP <= 20)
+                {
+                    bool wasHealed = Player2.Heal();
+                    if (wasHealed)
+                    {
+                        Console.WriteLine("Octopus postava bola uzdravena.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Octopus nema manu nebola uzdravena.");
+                    }
+
+                }
+
+                Random random = new Random();
+                int randomNumber = random.Next(0, 100);
+                
+                if (randomNumber <= 5)
+                {
+                    Player.RefilMana(10);
+                    Console.WriteLine("TitaniusOcelChuj doplnena mana.");
+                }
+
+                
+                int Number = random.Next(0, 100);
+                if (randomNumber <= 5)
+                {
+                      Player2.RefilMana(10);
+                      Console.WriteLine("Octopus doplnena mana.");
+                }
             }
-            
+
             if (Player.HP < 0)
             {
-                return Player.Mana;
+                Console.WriteLine("Octopus vyhral");
             }
-            else if (Player2.HP < 0) 
-            { 
-            }
+            
             if (Player2.HP < 0)
             {
+                      
                 Console.WriteLine("TitaniusOcelChuj vyhral");
             }
 
+            
         }
         
         
